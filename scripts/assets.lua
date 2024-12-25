04e079d057c40f5c6e18b6f4ccefe5712fe97e8a
Richard Egeli <richard.stromland.egeli@intoto.io>
2024-12-25 21:54:15 +0100

    chore: simple test for loading a spritesheet

---
+++ scripts/assets.lua
@@ -0,0 +1,74 @@
+---@type Animation[]
+local human_animations = {
+	{
+		name = "idle_down",
+		loop = false,
+		frames = {
+			{
+				row = 1,
+				column = 0,
+				duration = 0,
+				invert = true,
+			},
+		},
+	},
+	{
+		name = "idle_left",
+		loop = false,
+		frames = {
+			{
+				column = 1,
+				row = 1,
+				duration = 0,
+				invert = false,
+			},
+		},
+	},
+	{
+		name = "idle_right",
+		loop = false,
+		frames = {
+			{
+				column = 1,
+				row = 2,
+				duration = 0,
+				invert = false,
+			},
+		},
+	},
+	{
+		name = "idle_up",
+		loop = false,
+		frames = {
+			{
+				column = 1,
+				row = 3,
+				duration = 0,
+				invert = false,
+			},
+		},
+	},
+	{
+		name = "walk_down",
+		loop = true,
+		frames = {},
+	},
+	{
+		name = "walk_right",
+		loop = true,
+		frames = {},
+	},
+	{
+		name = "walk_left",
+		loop = true,
+		frames = {},
+	},
+	{
+		name = "walk_up",
+		loop = true,
+		frames = {},
+	},
+}
+
+PLAYER_SPRITE = AssetLoader.load_spritesheet("assets/sprite/character_1-8.png", 12, 9, human_animations)
+CHAR_SPRITE = AssetLoader.load_spritesheet("assets/sprite/character_17-24.png", 12, 9, human_animations)
