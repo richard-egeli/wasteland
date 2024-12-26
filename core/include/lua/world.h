131ebed573548776f0302c7fc0b3d4504792e1fc
Richard Egeli <richard.stromland.egeli@intoto.io>
2024-12-26 20:06:58 +0100

    refactor: remove entity logic from world

--- core/include/lua/world.h
+++ core/include/lua/world.h
@@ -10,19 +10,6 @@ typedef struct lua_State lua_State;
 extern SceneGraph* worlds[1024];
 extern size_t worlds_count;
 
-typedef struct Entity {
-    int id;
-    int self_ref;
-    int update_ref;
-    int render_ref;
-    int parent;
-    lua_State* L;
-} Entity;
-
-typedef struct World {
-    SceneGraph* graph;
-} World;
-
 void register_world_api(lua_State* L);
 
 #endif  // INCLUDE_LUA_WORLD_H_
