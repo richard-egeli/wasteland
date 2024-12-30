13eba717bd5785a4dc99a8c0908a5453b8d11539
Richard Egeli <richard.stromland.egeli@intoto.io>
2024-12-30 21:06:25 +0100

    chore: update main

--- core/main.c
+++ core/main.c
@@ -16,6 +16,7 @@
 #include "box2d/box2d.h"
 #include "global.h"
 #include "lua/asset_loader.h"
+#include "lua/dynamic_body.h"
 #include "lua/entity.h"
 #include "lua/input.h"
 #include "lua/world.h"
@@ -43,6 +44,7 @@ static void lua_init(const char* file) {
     register_asset_loader_api(L);
     register_input_api(L);
     register_entity_api(L);
+    register_dynamic_body_api(L);
 
     lua_pushcfunction(L, error_handler);  // Push error handler
     int error_handler_index = lua_gettop(L);
@@ -74,28 +76,36 @@ int main(void) {
     lua_init("scripts/main.lua");
 
     for (int i = 0; i < worlds_count; i++) {
-        scene_graph_compute_positions(worlds[i]);
+        scene_graph_compute_positions(worlds[i]->graph);
     }
 
-    uint64_t count       = 0;
-    double total_time    = 0.0;
+    uint64_t count    = 0;
+    double total_time = 0.0;
 
-    threadpool pool      = thpool_init(16);
-    b2WorldDef world_def = b2DefaultWorldDef();
-    b2WorldId world      = b2CreateWorld(&world_def);
+    threadpool pool   = thpool_init(16);
 
     while (!WindowShouldClose()) {
         for (int i = 0; i < worlds_count; i++) {
-            scene_graph_update(worlds[i]);
-            scene_graph_compute_positions(worlds[i]);
-            scene_graph_ysort_parallel(worlds[i], pool);
+            scene_graph_update(worlds[i]->graph);
+
+            b2World_Step(worlds[i]->id, GetFrameTime(), 4);
+            for (int j = 0; j < dynamic_body_movements_length; j++) {
+                Movement m = dynamic_body_movements[j];
+                b2Vec2 p   = b2Body_GetPosition(m.body_id);
+                scene_graph_position_set(worlds[i]->graph, m.node, (Position){p.x, p.y});
+            }
+
+            dynamic_body_movements_length = 0;
+
+            scene_graph_compute_positions(worlds[i]->graph);
+            scene_graph_ysort_parallel(worlds[i]->graph, pool);
         }
 
         BeginDrawing();
         ClearBackground(WHITE);
 
         for (int i = 0; i < worlds_count; i++) {
-            scene_graph_render(worlds[i]);
+            scene_graph_render(worlds[i]->graph);
         }
 
         DrawFPS(10, 10);
