f31d0204fb1a498704a27cb5e6929640fa8d0b99
Richard Egeli <richard.stromland.egeli@intoto.io>
2025-01-01 17:46:29 +0100

    fix: use environment table properly

--- core/src/lua/utils.c
+++ core/src/lua/utils.c
@@ -17,27 +17,19 @@ bool is_custom_property(const char* name, const char* filter[], size_t length) {
 }
 
 void setup_metatable(lua_State* L, const char* name, int idx, const char* filter[], size_t length) {
-    // Create new metatable for this instance
-    lua_createtable(L, 0, 2);
-
-    // Set up inheritance from Entity metatable
     luaL_getmetatable(L, name);
-    lua_pushstring(L, "__index");
-    lua_pushvalue(L, -2);
-    lua_settable(L, -4);
+    lua_setmetatable(L, -2);
 
-    // Copy custom properties
+    lua_newtable(L);
     lua_pushnil(L);
     while (lua_next(L, idx) != 0) {
         const char* name = lua_tostring(L, -2);
         if (is_custom_property(name, filter, length)) {
             lua_pushvalue(L, -2);  // key
             lua_pushvalue(L, -2);  // value
-            lua_settable(L, -5);   // set in metatable
+            lua_rawset(L, -5);     // set in env table
         }
         lua_pop(L, 1);
     }
-
-    lua_pop(L, 1);  // Pop Entity metatable
-    lua_setmetatable(L, -2);
+    lua_setfenv(L, -2);
 }
