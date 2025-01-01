4a162dc67d55c66e43d1cbda853bb999df8fb4fa
Richard Egeli <richard.stromland.egeli@intoto.io>
2025-01-01 17:47:25 +0100

    refactor: update type information for lua

--- scripts/types.d.lua
+++ scripts/types.d.lua
@@ -6,21 +6,45 @@
 ---@field get_position fun(self:Entity): x:number, y:number
 ---@field set_position fun(self:Entity, x:number,y:number)
 
+---@class Body : Entity
+---@field get_type fun(self:Body): integer
+
+---@class Dynamic : Body
+---@field move fun(self: Dynamic, x: number, y:number)
+
+---@class Static : Body
+
+---@class SpriteDef
+---@field sprite integer
+---@field row? integer
+---@field col? integer
+
 ---@class EntityDef
 ---@field sprite? number
 ---@field load? fun(self: Entity)
 ---@field update? fun(self: Entity)
 ---@field parent? Entity
 
----@class DynamicBodyDef
----@field x number
----@field y number
----@field parent? any
+---@class DynamicDef
+---@field x? number
+---@field y? number
+---@field parent? Entity
+---@field on_collision_enter? fun(self:Dynamic, other:any)
+---@field on_collision_exit? fun(self:Dynamic, other:any)
+
+---@class StaticDef
+---@field x? number
+---@field y? number
+---@field parent? Entity
+---@field on_collision_enter? fun(self:Static, other:any)
+---@field on_collision_exit? fun(self:Static, other:any)
 
 ---@class World
 ---@field new fun(): World
 ---@field create_entity fun(self:World, entity: EntityDef): any
----@field create_dynamic_body fun(self:World, definition: DynamicBodyDef): any
+---@field create_dynamic_body fun(self:World, definition: DynamicDef): any
+---@field create_static_body fun(self:World, definition:StaticDef): any
+---@field create_sprite fun(self:World, definition:SpriteDef): any
 
 ---@class Input
 ---@field is_down fun(key:integer): boolean
