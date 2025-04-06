d5ce0777f76ea29588f7e930f8a773132387277f
Richard <richard@cloud.egeli>
2025-04-06 07:27:59 +0200

    chore: simple rendering of map format

--- core/main.c
+++ core/main.c
@@ -24,9 +24,15 @@
 #include "lua/sprite.h"
 #include "lua/static_body.h"
 #include "lua/world.h"
+#include "mpx/layer.h"
+#include "mpx/mpx.h"
+#include "mpx/objectgroup.h"
+#include "mpx/tiledata.h"
+#include "mpx/tileset.h"
 #include "scene-graph/parallel-graph-sort.h"
 #include "scene-graph/scene-graph.h"
 #include "thpool/thpool.h"
+#include "vec/vec.h"
 
 static int error_handler(lua_State* L) {
     const char* msg = lua_tostring(L, 1);
@@ -73,6 +79,128 @@ static void lua_init(const char* file) {
     lua_pcall(L, 0, 0, error_handler_index);
 }
 
+// Define the comparison function for sorting
+int compare_by_ypos(const void* a, const void* b) {
+    mpx_object_t* obj_a = *(mpx_object_t**)a;
+    mpx_object_t* obj_b = *(mpx_object_t**)b;
+
+    // Sort by ypos in ascending order
+    if (obj_a->ypos < obj_b->ypos) return -1;
+    if (obj_a->ypos > obj_b->ypos) return 1;
+    return 0;
+}
+
+static void map_sort_objects(mpx_t* mpx) {
+    for (size_t i = 0; i < vec_length(mpx->objectgroups); i++) {
+        mpx_objectgroup_t* group = mpx->objectgroups[i];
+
+        // Sort the objects in this group by ypos
+        qsort(group->objects, vec_length(group->objects), sizeof(mpx_object_t*), compare_by_ypos);
+    }
+}
+
+static void map_draw_objects(mpx_t* mpx, Texture* textures) {
+    for (size_t i = 0; i < vec_length(mpx->objectgroups); i++) {
+        mpx_objectgroup_t* group = mpx->objectgroups[i];
+        if (!group->visible) continue;
+
+        for (size_t j = 0; j < vec_length(group->objects); j++) {
+            mpx_object_t* object   = group->objects[j];
+            uint16_t tileset_id    = mpx_tile_tileset_get(object->tile);
+            uint16_t tile_id       = mpx_tile_index_get(object->tile);
+            mpx_tileset_t* tileset = mpx->tilesets[tileset_id];
+            Rectangle sourceRect   = {
+                  .x      = (tile_id % tileset->columns) * tileset->tilewidth,
+                  .y      = (tile_id / tileset->columns) * tileset->tileheight,
+                  .width  = tileset->tilewidth,
+                  .height = tileset->tileheight,
+            };
+
+            printf("%d\n", tileset_id);
+            Texture texture  = textures[tileset_id];
+            Vector2 position = {object->xpos, object->ypos - tileset->tileheight};
+            DrawTextureRec(texture, sourceRect, position, WHITE);
+        }
+    }
+}
+
+static void map_init(const char* file) {
+    Texture textures[128] = {0};
+    mpx_t* mpx            = mpx_load(file);
+    if (mpx == NULL) {
+        TraceLog(LOG_ERROR, "MPX: [%s] Map failed to load", file);
+        exit(1);
+    }
+
+    TraceLog(LOG_INFO,
+             "MPX: [%s] Map loaded successfully (Tilesets %zu | Layers %zu)",
+             file,
+             vec_length(mpx->tilesets),
+             vec_length(mpx->layers));
+
+    const char* root = "../mpxtiledconverter/assets";
+    char* path       = malloc(1024);
+    for (size_t i = 0; i < vec_length(mpx->tilesets); i++) {
+        mpx_tileset_t* tileset = mpx->tilesets[i];
+        snprintf(path, 1024, "%s/%s", root, tileset->filepath);
+        textures[i] = LoadTexture(path);
+    }
+
+    // sleep(0xFFFFFFFF);
+    free(path);
+
+    map_sort_objects(mpx);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        size_t max = 0;
+        size_t dw  = 0;
+        size_t tw  = 0;
+        size_t id  = 0;
+
+        for (size_t i = 0; i < vec_length(mpx->layers); i++) {
+            mpx_layer_t* layer   = mpx->layers[i];
+            mpx_tiledata_t* data = layer->data;
+            if (!layer->visible) continue;
+
+            for (int j = 0; j < data->length; j++) {
+                uint32_t tile = data->tiles[j];
+                if (tile == MPX_TILE_NULL) continue;
+
+                uint16_t tileset_id    = mpx_tile_tileset_get(tile);
+                uint16_t tile_id       = mpx_tile_index_get(tile);
+                mpx_tileset_t* tileset = mpx->tilesets[tileset_id];
+
+                int xpos               = (j % data->width) * mpx->tilewidth;
+                int ypos               = (j / data->width) * mpx->tileheight;
+                Texture texture        = textures[tileset_id];
+
+                Rectangle sourceRect   = {
+                      .x      = (tile_id % tileset->columns) * tileset->tilewidth,
+                      .y      = (tile_id / tileset->columns) * tileset->tileheight,
+                      .width  = tileset->tilewidth,
+                      .height = tileset->tileheight,
+                };
+
+                // Position on screen remains the same
+                Vector2 position = {
+                    .x = xpos,
+                    .y = ypos,
+                };
+
+                DrawTextureRec(texture, sourceRect, position, WHITE);
+            }
+        }
+
+        map_draw_objects(mpx, textures);
+
+        DrawFPS(10, 10);
+        EndDrawing();
+    }
+}
+
 int main(void) {
     InitWindow(1280, 720, "Witch");
     SetWindowState(FLAG_WINDOW_TOPMOST | FLAG_WINDOW_RESIZABLE);
@@ -81,6 +209,8 @@ int main(void) {
     srand((unsigned)time(NULL));
 
     lua_init("scripts/main.lua");
+    map_init("../mpxtiledconverter/assets/fuck.mpx");
+    return 0;
 
     for (int i = 0; i < worlds_count; i++) {
         scene_graph_compute_positions(worlds[i]->graph);
